58fed5fc6e8ea5cf301985fa233c32516ffa63c8
polinakoroleva <polinakorolevaaaaa@gmail.com>
2024-03-01 15:22:53 +0700

    fix text size

--- src/app/components/MainPage/Header.tsx
+++ src/app/components/MainPage/Header.tsx
@@ -47,7 +47,7 @@ const Header: FC = () => {
       <div className="bg-white dark:bg-gray-700 overflow-hidden relative">
         <div className="container relative z-10 flex items-start py-32 mx-auto">
           <div className="relative ml-10  w-1/2 z-10 flex flex-col items-start">
-            <h1 className="font-extrabold text-balance leading-tight text-gray-800 dark:text-gray-100 text-6xl sm:text-8xl">
+            <h1 className="font-extrabold text-balance leading-tight text-gray-800 dark:text-gray-100 text-5xl sm:text-8xl">
               {t('header.title')}
             </h1>
             <StartButton />
