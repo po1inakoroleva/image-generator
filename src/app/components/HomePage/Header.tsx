699e57ec720586b099460dbbd9ee2b0a913c9678
Polina Koroleva <122518073+po1inakoroleva@users.noreply.github.com>
2024-03-13 00:54:47 +0700

    Update Header.tsx

--- src/app/components/HomePage/Header.tsx
+++ src/app/components/HomePage/Header.tsx
@@ -11,7 +11,7 @@ const Header: FC = () => {
       <nav className="container px-3 py-4 mx-auto md:px-3">
         <div className="items-center justify-center md:flex">
           <div className="items-center md:flex">
-            <a href='/' className="flex mx-3 text-lg text-indigo-500 uppercase cursor-pointer hover:text-gray-300">
+            <a href='/' className="flex text-lg text-indigo-500 uppercase cursor-pointer hover:text-gray-300">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
               </svg>
