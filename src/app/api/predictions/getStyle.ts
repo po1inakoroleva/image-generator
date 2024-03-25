f7e1885a0c31621c3a3524535a1a6f144529ddbe
polinakoroleva <polinakorolevaaaaa@gmail.com>
2024-03-25 15:31:21 +0700

    add style prompts

---
+++ src/app/api/predictions/getStyle.ts
@@ -0,0 +1,8 @@
+const colors: readonly string[] = ['blue, green, white', 'green, white, pastel yellow', 'blue, pink, white, pastel yellow', 'pink, white, pastel yellow', 'pink, green, white', 'blue, pink, white'];
+
+const getStyle = () => {
+  const colorIndex = Math.floor(Math.random() * (colors.length - 1));
+  return `drawing line, minimalism, pastel colors, ${colors[colorIndex]}`;
+};
+
+export default getStyle;
--- src/app/api/predictions/route.ts
+++ src/app/api/predictions/route.ts
@@ -1,11 +1,14 @@
 import Replicate from 'replicate';
 
+import getStyle from './getStyle.ts';
+
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN,
 });
 
 export async function POST(req: Request) {
   const data = await req.formData();
+  const style = getStyle();
 
   const prediction = await replicate.predictions.create({
     // Pinned to a specific version of Stable Diffusion
@@ -13,7 +16,7 @@ export async function POST(req: Request) {
     version: '8beff3369e81422112d93b89ca01426147de542cd4684c244b673b105188fe5f',
 
     // This is the text prompt that will be submitted by a form on the frontend
-    input: { prompt: data.get('prompt') },
+    input: { prompt: `${style} ${data.get('prompt')}` },
   });
 
   if (prediction?.error) {
