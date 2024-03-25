320d1d070164fcb367ac3203693e068999836ebd
polinakoroleva <polinakorolevaaaaa@gmail.com>
2024-03-25 16:18:15 +0700

    fix getStyle.ts

--- src/app/api/predictions/getStyle.ts
+++ src/app/api/predictions/getStyle.ts
@@ -1,4 +1,4 @@
-const colors: readonly string[] = ['blue, green, white', 'green, white, pastel yellow', 'blue, pink, white, pastel yellow', 'pink, white, pastel yellow', 'pink, green, white', 'blue, pink, white'];
+const colors: readonly string[] = ['blue, green, white', 'green, white, pastel yellow', 'blue, pink, white, pastel yellow', 'pink, white, pastel yellow', 'pastel pink, green, white', 'blue, pastel pink, white'];
 
 const getStyle = () => {
   const colorIndex = Math.floor(Math.random() * (colors.length - 1));
